ff057d7c19803c2eeb21a9257ae2a45f840e2879
GreatAlex <alexguerillot1@gmail.com>
2023-06-28 10:46:36 +0200

    [feat/master : premier js]
    [essais gde icone en js]

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const meteoMain = document.getElementById(gd_Meteo_Icone);
+console.log(meteoMain);
+const gdIcone = document.createElement("img");
+
+gdIcone.src = "/assets/meteo/gd_soleil.png/";
+
+meteoMain.appendChild(gdIcone);
