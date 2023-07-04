426cf2a7e3ceb2fd8b92703ca48987c0457ab3fb
GreatAlex <alexguerillot1@gmail.com>
2023-07-04 16:05:47 +0200

    [feat/ master : fonctions]
    [fetch quasi ok : fonction météo à temporiser]

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
+/**
+ *
+ *                     INSERTION DE LA GRANDE ICONE METEO - PAGE MAIN
+ *
+ **/
+
+const firstC = document.getElementById("gdMeteoIcone").firstChild;
+const gdIcone = document.createElement("img");
+gdIcone.src = "/assets/meteo/gd_soleil.png";
+firstC.before(gdIcone); // insertion gde img meteo avant span //
+
+/**
+ *
+ *                      "L'ARBRE A FONCTIONS" - STRUCTURE PRINCIPALE PRG - PAGE MAIN
+ *
+ **/
+
 function getData() {
-  getVille();
-  getLatitudeLongitude(ville);
-  getMeteo();
+  let ville = getVille();
+  let latlon = getLatitudeLongitude(ville);
+  let result = getMeteo(latlon);
 }
 const validateVille = document.querySelector("button");
 const searchVille = document.querySelector("input");
@@ -10,19 +27,18 @@ validateVille.addEventListener("click", getData);
 
 /**
  *
- *                          RECUP VALEUR CHAMP POUR RECHERCHE API GEOCODING
+ *                       RECUP VALEUR CHAMP POUR RECHERCHE API GEOCODING
  *
  **/
 
-function getVille() {
-  let ville = searchVille.value; //recup valeur ville saisie si click bouttp*/
-
-  if (!ville) {
+function getVille(ville) {
+  ville = searchVille.value;
+  if (ville == "") {
     ville = "Besancon"; //valeur par défaut : besançon
-    searchVille.placeholder = ville;
+    return ville;
+  } else {
+    return ville;
   }
-
-  let villeLatLon = getLatitudeLongitude(ville);
 }
 
 /**
@@ -32,25 +48,26 @@ function getVille() {
  **/
 
 /*{country code}*/
+//creation objet latlon, easy to pass values
 
-function getLatitudeLongitude(ville) {
-  fetch(
+async function getLatitudeLongitude(ville) {
+  await fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${ville},fr&limit=5&appid=0ecf229967bee135b64207c0a18df389`
   )
-    .then((response) => response.json()) // convert to json
-    /*.then((json) => console.log(json)) //print data to console*/
-
+    .then((response) => response.json()) // convert to json//
+    /*.then((json) => console.log(json)) //print data to console// */
     .then((response) => {
-      for (let i = 0; i < response.length; i++) {
-        let latiVille = response[0].lat;
-        let longiVille = response[0].lon;
-        let result = {
-          latitude: latiVille,
-          longitude: longiVille,
-        };
-        return result;
-      }
+      i = 0;
+      //for (let i = 0; i < response.length; i++) { qd je traiterai une liste de résultat//
+      let latiVille = response[i].lat;
+      let longiVille = response[i].lon;
+
+      latlon = {
+        latitude: latiVille,
+        longitude: longiVille,
+      };
     });
+  return latlon;
 }
 
 /**
@@ -59,47 +76,19 @@ function getLatitudeLongitude(ville) {
  *
  **/
 
-function getMeteo(villeLatLon) {
-  fetch(
-    `http://api.openweathermap.org/data/2.5/forecast?lat=${villeLatLon.latitude}&${villeLatLon.longitude}&appid=0ecf229967bee135b64207c0a18df389`
-  )
-    .then((response) => response.json()) // convert to json
-    .then((json) => console.log(json)) //print data to console
-    .catch((err) => console.log("Request Failed", err)); // Catch errors
-  console.log(latiVille);
+async function getMeteo(latlon) {
+  console.log(latlon);
+  await fetch(
+    `http://api.openweathermap.org/data/2.5/forecast?lat=${latlon.latitude}&${latlon.longitude}&appid=0ecf229967bee135b64207c0a18df389`
+  );
+  console
+    .log(latlon.latitude)
+    .then((response) => response.json()) // convert to json//
+
+    .then((response) => {});
+
+  //.then((json) => console.log(json)); print data to console//
+  //.catch((err) => console.log("Request Failed", err));  Catch errors//
+
+  return result;
 }
-
-/**
- *
- *       INSERTION DE LA GRANDE ICONE METEO - PAGE MAIN
- *
- **/
-
-const firstC = document.getElementById("gdMeteoIcone").firstChild;
-const gdIcone = document.createElement("img");
-gdIcone.src = "/assets/meteo/gd_soleil.png";
-firstC.before(gdIcone); // insertion gde img meteo avant span //
-
-/*
-
-console.log();
-.then.catch((err) => console.log("Request Failed", err)); // Catch errors
-
-
-fetch(
-  `http://api.openweathermap.org/geo/1.0/direct?q=${ville},fr&limit=5&appid=0ecf229967bee135b64207c0a18df389`,
-  console.log(ville)
-  )
-  .then((response) => response.json()) // convert to json
-  .then((json) => console.log(json)) //print data to console
-  .catch((err) => console.log("Request Failed", err)); // Catch errors
-  
-  let response;
-  
-  fetch(
-    "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=0ecf229967bee135b64207c0a18df389"
-    )
-    .then((response) => response.json()) // convert to json
-    .then((json) => console.log(json)) //print data to console
-    .catch((err) => console.log("Request Failed", err)); // Catch errors
-    */
